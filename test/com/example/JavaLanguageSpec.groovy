a33fe3d37467c1e0197af28a6656d86dc6f02192
Ken Sipe <kensipe@gmail.com>
2017-10-03 10:35:48 -0700

    init project

---
+++ test/com/example/JavaLanguageSpec.groovy
@@ -0,0 +1,28 @@
+package com.example
+
+import spock.lang.IgnoreIf
+import spock.lang.Requires
+import spock.lang.Specification
+
+/**
+ *
+ * @author kensipe
+ */
+class JavaLanguageSpec extends Specification {
+
+    @IgnoreIf({ !jvm.java8 })
+    def "java 8 feature"() {
+        def friends = ["Brian", "Nate", "Neal", "Raju", "Sara", "Scott", "Venkat"]
+
+        expect:
+        friends.stream().findFirst() != null
+    }
+
+    @Requires({ jvm.java8 })
+    def "java 8 feature java8 required"() {
+        def friends = ["Brian", "Nate", "Neal", "Raju", "Sara", "Scott", "Venkat"]
+
+        expect:
+        friends.stream().findFirst() != null
+    }
+}
